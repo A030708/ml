5cc50d9e97810dcde68ec530e41c315f250f53eb
A030708 <abhiramkm2003@gmail.com>
2026-02-17 15:25:25 +0530

    Integrate matplotlib for SVM prediction visualization
    Added plotting functionality to visualize SVM predictions.

--- 8. SVM.py
+++ 8. SVM.py
@@ -1,15 +1,22 @@
 import numpy as np
 from sklearn.svm import SVC
 from sklearn.preprocessing import StandardScaler
+import matplotlib.pyplot as plt
 
 X = np.array([[150,22],[155,25],[158,28],[160,30],[162,35],[148,20],[152,27],[157,32],[159,26],[161,29],
               [168,22],[170,25],[172,28],[175,30],[178,35],[180,40],[182,33],[176,27],[174,29],[169,24]])
 y = np.array([0]*10 + [1]*10)
-
 scaler = StandardScaler()
 X = scaler.fit_transform(X)
-
 model = SVC(kernel='linear').fit(X, y)
 new = scaler.transform([[165, 26]])
-
 print("Prediction:", "Man" if model.predict(new)[0] else "Woman")
+
+#plot
+for i in range(len(X)):
+    if y[i] == 0:
+        plt.scatter(X[i][0], X[i][1], color='red')
+    else:
+        plt.scatter(X[i][0], X[i][1], color='blue')
+plt.scatter(new[0][0], new[0][1], color='orange', marker='*', s=120)
+plt.show()
