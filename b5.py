fb8f77161bc140445a17e399dc248a7fae4f0e60
A030708 <abhiramkm2003@gmail.com>
2026-02-27 12:21:02 +0530

    Update b5.py

--- b5.py
+++ b5.py
@@ -1,4 +1,6 @@
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.tree import plot_tree
+import matplotlib.pyplot as plt
 
 # Training data: [attendance %, study hours, sleep hours]
 X = [
@@ -11,9 +13,18 @@ y = ["FAIL","FAIL","FAIL","FAIL","PASS",
      "PASS","PASS","PASS","FAIL","FAIL",
      "PASS","PASS","PASS","PASS","PASS"]
 
-model = RandomForestClassifier()
+# Train Random Forest
+model = RandomForestClassifier(n_estimators=5, random_state=0)
 model.fit(X, y)
 
+# Prediction
 pred = model.predict([[85,5,7]])
-
 print("Result:", pred[0])
+
+# Plot one tree from the forest
+plt.figure(figsize=(8,6))
+plot_tree(model.estimators_[0],
+          feature_names=['attendance','study_hour','sleep'],
+          class_names=['FAIL','PASS'],
+          filled=True)
+plt.show()
